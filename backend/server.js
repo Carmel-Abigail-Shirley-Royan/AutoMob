a4d99d9d3f539bb6db7149dd845da4bbb6f33020
Carmel-Abigail-Shirley-Royan <carmelabigailroyan@gmail.com>
2025-06-22 11:21:59 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import bookingRoutes from "./routes/bookings.js";
+import bookingRoutes from "./routes/booking.js";
 
 dotenv.config(); // Load variables from .env
 
